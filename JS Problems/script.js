b48ed96b68532852c25c5d004f4661e51bc8f653
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-27 22:39:16 +0530

    fix: solved value interchange in js

---
+++ JS Problems/script.js
@@ -0,0 +1,17 @@
+let a = 30;
+let b= 50;
+console.log(`a = ${a} , b = ${b}`);
+
+
+// interChange in this values 
+
+
+// b = a+b;
+// a = b-a;
+// b = b-a;
+
+// console.log(`a = ${a} , b = ${b}`);
+
+
+[a,b]=[b,a]
+console.log(`a = ${a} , b = ${b}`);
