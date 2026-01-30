999cd0302b20250dfc56aa4a540bd94186ec42f0
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-01-30 08:51:39 +0530

    Implement string reversal logic without using reverse()

---
+++ JS Problems/reverse string/script.js
@@ -0,0 +1,8 @@
+function reverseStr(str){
+    let res = "";
+    for(let i = str.length - 1 ; i >= 0 ;i--){
+        res += str[i];
+    }
+    return res;
+}
+console.log(reverseStr("javascript"));
