eefc031e09e21c26de964d81d9fcbc5cdaa575ae
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-05 21:53:29 +0530

    fix : camra close functionaltiy

--- permissions-project/script.js
+++ permissions-project/script.js
@@ -1,10 +1,13 @@
 const allowBtn = document.getElementById("allowBtn");
+const closeBtn = document.getElementById('closeBtn')
 const video = document.getElementById("video");
 const statusText = document.getElementById("status");
+let stream;
 
 allowBtn.addEventListener("click", () => {
   navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
+    .then(s => {
+      stream = s;
       video.srcObject = stream;
       statusText.innerText = "Camera permission granted ✅";
       statusText.style.color = "green";
@@ -14,4 +17,13 @@ allowBtn.addEventListener("click", () => {
       statusText.style.color = "red";
       console.error(err);
     });
+});
+closeBtn.addEventListener("click", () => {
+ if(stream) {
+  stream.getTracks().forEach(track => track.stop());
+  video.srcObject = null;
+
+  statusText.innerText = "Camra closed";
+  statusText.style.color = 'red';
+ }
 });
