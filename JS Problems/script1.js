5f9a6ded4502220511959caa34b8232a53df2c4b
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-07 22:19:20 +0530

    solved : count character frequency

--- JS Problems/script1.js
+++ JS Problems/script1.js
@@ -1,13 +1,20 @@
-let word = "or coding logic";
-removeDublicate(word);
-function removeDublicate(val){
-    let seen = {}
-    let result = "";
-    for(char of val){
-        if(!seen[char]){
-             result+=char;
-             seen[char] = true;
-        }
-    }
-    console.log(result);
+// let word = "or coding logic";
+// removeDublicate(word);
+// function removeDublicate(val){
+//     let seen = {}
+//     let result = "";
+//     for(char of val){
+//         if(!seen[char]){
+//              result+=char;
+//              seen[char] = true;
+//         }
+//     }
+//     console.log(result);
+// }
+function freq(str){
+  let obj = {};
+  for(let ch of str){
+    obj[ch] = (obj[ch] || 0) + 1;
+  }
+  return obj;
 }
