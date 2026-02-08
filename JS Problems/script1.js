3af41608f594bbe79d57c334151d456a497c255e
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-08 22:20:54 +0530

    deep clone

--- JS Problems/script1.js
+++ JS Problems/script1.js
@@ -11,10 +11,46 @@
 //     }
 //     console.log(result);
 // }
-function freq(str){
-  let obj = {};
-  for(let ch of str){
-    obj[ch] = (obj[ch] || 0) + 1;
+// function freq(str){
+//   let obj = {};
+//   for(let ch of str){
+//     obj[ch] = (obj[ch] || 0) + 1;
+//   }
+//   return obj;
+// }
+function deepClone(obj){
+
+  if(obj === null || typeof obj !== "object"){
+    return obj;
   }
-  return obj;
+
+  if(Array.isArray(obj)){
+    let copy = [];
+    for(let i=0;i<obj.length;i++){
+      copy[i] = deepClone(obj[i]);
+    }
+    return copy;
+  }
+
+  let copy = {};
+  for(let key in obj){
+    copy[key] = deepClone(obj[key]);
+  }
+
+  return copy;
 }
+
+const user = {
+  name: "Prince",
+  skills: ["JS","React"],
+  address: {
+    city: "Indore"
+  }
+};
+
+const newUser = deepClone(user);
+
+newUser.address.city = "Delhi";
+
+console.log(user.address.city); // Indore
+console.log(newUser.address.city); // Delhi
