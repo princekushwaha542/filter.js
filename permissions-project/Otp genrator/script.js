4868cf5d348b41e8c3bccafe58991f0f17c6a338
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-09 22:05:49 +0530

    feat: implement otp-based

---
+++ permissions-project/Otp genrator/script.js
@@ -0,0 +1,28 @@
+let generatedOTP;
+
+function sendOTP() {
+  const phone = document.getElementById("phone").value;
+
+  if(phone.length !== 10 || isNaN(phone)) {
+    alert("Enter valid mobile number");
+    return;
+  }
+
+  generatedOTP = Math.floor(100000 + Math.random() * 900000);
+  console.log("OTP:", generatedOTP); 
+
+  document.getElementById("otpBox").classList.remove("hidden");
+  document.getElementById("msg").innerText = "OTP Sent Successfully ✅";
+}
+
+function verifyOTP() {
+  const userOTP = document.getElementById("otpInput").value;
+
+  if(userOTP == generatedOTP) {
+    document.getElementById("msg").innerText = "OTP Verified 🎉 Login Success";
+    document.getElementById("msg").style.color = "green";
+  } else {
+    document.getElementById("msg").innerText = "Invalid OTP ❌";
+    document.getElementById("msg").style.color = "red";
+  }
+}
